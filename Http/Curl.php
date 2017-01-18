6bb6617d6e1b1bfb4b76a0a3d3ff4bef9ebba17b
adadgio <romain.bruckert.esc@gmail.com>
2017-01-18 17:03:23 +0100

    Added a PUT method shorthand in Http\Curl utility.

--- Http/Curl.php
+++ Http/Curl.php
@@ -140,6 +140,39 @@ class Curl
         return $this->handleResponse($result, $code, $error);
     }
 
+    public function put($url, array $putFields = array())
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->headers);
+
+        // post field must be differently formated
+        // depending on content type
+        switch ($this->contentType) {
+            case static::JSON:
+                $putFields = json_encode($putFields);
+            break;
+            case static::FORM_URLENCODED:
+                $putFields = http_build_query($putFields);
+            break;
+            case static::FORM_MULTIPART:
+                // post fields stay as array here
+            break;
+            default:
+                // nothing special
+            break;
+        }
+
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $putFields);
+
+        $result = curl_exec($this->curl);
+        $code = curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
+        $error = curl_error($this->curl);
+        curl_close($this->curl);
+
+        return $this->handleResponse($result, $code, $error);
+    }
+
     private function handleResponse($result, $code, $error)
     {
         $response = null;
